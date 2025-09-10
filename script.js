cee9dc5568e6209587ed916d7dd5d65300d83655
Desirée Strand <desireestrxnd@gmail.com>
2025-09-10 14:36:38 +0200

    add theme toggle

---
+++ script.js
@@ -0,0 +1,16 @@
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark");
+}
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
