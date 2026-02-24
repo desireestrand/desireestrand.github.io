70daa14de773fa4b98a9e6eafca45b6a5d60f6e5
Desirée Strand <desireestrxnd@gmail.com>
2026-02-24 13:48:25 +0100

    add hero styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,19 @@
+import s from './Hero.module.css'
+
 function Hero() {
 
   return (
     <>
-        <p>
+      <div className={s.hero}>
+        <h2>
             Hi! I'm Desirée, <br />
             a <span>Fullstack Developer</span> and <br />
             <span>UX enthusiast</span>, passionate about <br />
             crafting robust and meaningful <br />
             digital experiences <br />
             that create real value for users.
-        </p>
+        </h2>
+      </div>
     </>
   )
 }
