699a10f165b45b68cbb6b0c7a4df8a665f42b695
Desirée Strand <desireestrxnd@gmail.com>
2026-02-24 13:11:42 +0100

    add social link icons

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -2,9 +2,9 @@ function SocialLinks() {
 
   return (
     <>
-        <a href="mailto:desireestrxnd@gmail.com">E-mail</a>
-        <a href="https://www.linkedin.com/in/desir%C3%A9e-strand-a9ab7b159">LinkedIn</a>
-        <a href="https://github.com/desireestrand">GitHub</a>
+        <a href="mailto:desireestrxnd@gmail.com"><ion-icon name="mail"></ion-icon></a>
+        <a href="https://www.linkedin.com/in/desir%C3%A9e-strand-a9ab7b159"><ion-icon name="logo-linkedin"></ion-icon></a>
+        <a href="https://github.com/desireestrand"><ion-icon name="logo-github"></ion-icon></a>
     </>
   )
 }
