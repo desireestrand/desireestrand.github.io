f665948643223d1bff0f4de13a2513dfb5011769
Desirée Strand <desireestrxnd@gmail.com>
2026-02-25 15:13:22 +0100

    add contact content and styling

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,8 +1,13 @@
+import s from './ContactPage.module.css';
+
 function ContactPage() {
 
   return (
     <>
-      <h2>Contact</h2>
+      <div className={s.wrapper}>
+        <h2>Contact me!</h2>
+        <p>E-mail: <a href="mailto:desireestrxnd@gmail.com">desireestrxnd@gmail.com</a></p>
+      </div>
     </>
   )
 }
