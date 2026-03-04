9cb39bfa605ccb006cbf5b1a25c12c6932917f61
Desirée Strand <desireestrxnd@gmail.com>
2026-03-04 15:16:34 +0100

    add toggle menu for mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,24 @@
 import { NavLink } from "react-router-dom";
 import s from "./Navbar.module.css";
+import { useState } from "react";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   const navClass = ({ isActive }) => (isActive ? s.active : undefined);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
 
   return (
     <nav className={s.nav}>
-      <ul>
-        <li><NavLink to="/projects" className={navClass}>Projects</NavLink></li>
-        <li><NavLink to="/about" className={navClass}>About</NavLink></li>
-        <li><NavLink to="/contact" className={navClass}>Contact</NavLink></li>
+      <button className={s.menuIcon} onClick={toggleMenu} aria-label="Toggle menu">
+        <ion-icon name={isOpen ? "close" : "menu"}></ion-icon>
+      </button>
+
+      <ul className={`${s.navLinks} ${isOpen ? s.open : ""}`}>
+        <li><NavLink to="/projects" className={navClass} onClick={closeMenu}>Projects</NavLink></li>
+        <li><NavLink to="/about" className={navClass} onClick={closeMenu}>About</NavLink></li>
+        <li><NavLink to="/contact" className={navClass} onClick={closeMenu}>Contact</NavLink></li>
       </ul>
     </nav>
   );
