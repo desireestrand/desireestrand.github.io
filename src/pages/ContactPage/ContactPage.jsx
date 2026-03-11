afe38c2941e062dea740e22b25b95ac8c793d965
Desirée Strand <desireestrxnd@gmail.com>
2026-03-11 16:58:59 +0100

    add new contect  subtitle

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -5,7 +5,10 @@ function ContactPage() {
     <div className={s.wrapper}>
       <h2 className={s.title}>Let's connect!</h2>
       <p className={s.subtitle}>
-        I'm currently open for new opportunities and collaborations.
+        I'm currently seeking an internship (LIA) from November 2026 to May
+        2027, where I can apply my skills in Fullstack Development and UX/UI
+        Design while gaining practical experience and continuing to grow as a
+        developer. I would love to hear from you!
       </p>
 
       <div className={s.container}>
@@ -13,7 +16,12 @@ function ContactPage() {
           <span className={s.label}>E-mail</span>
           <span className={s.value}>desireestrxnd@gmail.com →</span>
         </a>
-        <a href="https://www.linkedin.com/in/desireestrand" target="_blank" rel="noreferrer" className={s.card}>
+        <a
+          href="https://www.linkedin.com/in/desireestrand"
+          target="_blank"
+          rel="noreferrer"
+          className={s.card}
+        >
           <span className={s.label}>LinkedIn</span>
           <span className={s.value}>Desirée Strand →</span>
         </a>
