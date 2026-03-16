5cdca4817f89b8edcb8eea857a8f64351232c120
Desirée Strand <desireestrxnd@gmail.com>
2026-03-16 14:06:43 +0100

    fix ionicons on mobile

--- src/components/BackToTop/BackToTop.jsx
+++ src/components/BackToTop/BackToTop.jsx
@@ -32,7 +32,7 @@ const BackToTop = () => {
           className={s.button}
           aria-label="Back to top"
         >
-          <ion-icon name="arrow-up"></ion-icon>
+          <ion-icon name="arrow-up" part="icon"></ion-icon>
         </button>
       )}
     </div>
